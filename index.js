7a1f88421557413f51090903d0e538d36705f6ca
Kamil Miśkowiec <kamilmiskowiec2401@gmail.com>
2021-04-09 15:50:13 +0200

    finished functions

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const axios = require('axios');
 const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 const USERS_URL =  'https://jsonplaceholder.typicode.com/users';
 
-
-function combine(posts,users){
-
-}
-
-let posts = [];
-let users = [];
-
 function countposts(users){
     let ans = [];
     users.forEach((user)=>{
@@ -19,40 +11,61 @@ function countposts(users){
     return ans;
 }
 
-
+function distance(lat1, lon1, lat2, lon2) {
+    let p = 0.017453292519943295;    // Math.PI / 180
+    let c = Math.cos;
+    let a = 0.5 - c((lat2 - lat1) * p)/2 + 
+            c(lat1 * p) * c(lat2 * p) * 
+            (1 - c((lon2 - lon1) * p))/2;
+  
+    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+  }
 
 function multititles(posts){
     let ans = [];
-    let titles = posts.map((post)=>post.title);
-    titles.sort();
-    for(let i=0;i<titles.length;i++){
-        multi=true;
-        if(i+1<titles.length && titles[i]===titles[i+1]){
-            multi=false;
-        }
-        if(i-1>=0 && titles[i]===titles[i-1]){
-            multi=false;
-        }
-        if(multi){
-            ans.push(titles[i]);
-        }
+    let titles = {};
+    posts.forEach((post)=>{
+        titles[post.title] = titles[post.title]===undefined?0:titles[post.title]+1;
+    });
+    for (const [key, value] of Object.entries(titles)) {
+        if(value>0) ans.push(key);
     }
     return ans;
+}
+
+function nearestuser(user,users){
+    let dist = null;
+    let nearest = null;
+    users.forEach((us)=>{
+        let disttemp = distance(us.address.geo.lat,us.address.geo.lng,user.address.geo.lat,user.address.geo.lng);
+        if(us.id!=user.id){
+            if(dist===null || dist>disttemp){
+                dist = disttemp;
+                nearest = us;
+            }
+        }
+    });
+    return nearest;
 }
 
 axios.all([
     axios.get(POSTS_URL),
     axios.get(USERS_URL)
   ]).then(axios.spread((response1, response2) => {
-    posts = response1.data;
-    users = response2.data;
+    let posts = response1.data;
+    let users = response2.data;
     users.forEach((user)=> user.posts=[]);
     posts.forEach((post)=>{
         users[post.userId-1].posts.push(post);
     });
-    countposts(users);
-    multititles(posts);
+    users.forEach((user)=>{
+        console.log("Nearest to "+user.username+" is "+nearestuser(user,users).username);
+    });
   })).catch(error => {
     console.log(error);
   });
 
+exports.nearestuser = nearestuser;
+exports.multititles = multititles;
+exports.distance = distance;
+exports.countposts = countposts;
